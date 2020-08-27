1c768b5bd876981757177ab8365a140ec06ebd99
Josh Raiborde <joshsrai@yahoo.com>
2020-08-27 12:56:18 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,3 @@
 INSERT INTO cats (name, age, breed) VALUES ('Maru', 3, 'Scottish Fold');
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 4, "M", "brown", "sweet", 1);
